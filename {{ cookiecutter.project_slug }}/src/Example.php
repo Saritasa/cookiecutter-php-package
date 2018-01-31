e99660f32de69ece82b79495148141ee4e8e7eef
Sergey Populov <sergey@saritasa.com>
2018-01-31 18:39:29 +0500

    Increase amount of PHP code to make GitHub consider it PHP project
    instead of Python

--- {{ cookiecutter.project_slug }}/src/Example.php
+++ {{ cookiecutter.project_slug }}/src/Example.php
@@ -7,4 +7,23 @@ namespace {{ cookiecutter.classes_namespace }};
  */
 class Example
 {
+    /**
+     * Create a new Example Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
 }
--- {{ cookiecutter.project_slug }}/tests/DemoTest.php
+++ {{ cookiecutter.project_slug }}/tests/DemoTest.php
@@ -16,4 +16,13 @@ class DemoTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    /**
+     * Check, that main project class can is accessible in tests, using autoload
+     */
+    public function testExample()
+    {
+        $example = new \{{ cookiecutter.classes_namespace }}\Example();
+        $this->assertEquals('test', $example->echoPhrase('test'));
+    }
 }
